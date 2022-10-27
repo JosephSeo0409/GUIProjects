8d55b651bf1df6d0bca08f246a8294d58ff25cf6
josseo23 <>
2022-10-27 17:53:26 +0000

    Initial commit

---
+++ DrawRubicks/GUIRubicks.java
@@ -0,0 +1,41 @@
+import javax.swing.*;    // For JFrame and JPanel
+import java.awt.*;       // For Color, Container, and GridLayout
+public class GUIRubicks{
+   public static void main(String[] args){
+      JFrame theGUI = new JFrame();
+      theGUI.setTitle("Rubicks Cube");
+      theGUI.setSize(300, 300);
+      theGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      JPanel panel1 = new JPanel(); 
+      panel1.setBackground(Color.white);
+      JPanel panel2 = new JPanel(); 
+      panel2.setBackground(Color.black);
+      JPanel panel3 = new JPanel(); 
+      panel3.setBackground(Color.gray);
+      JPanel panel4 = new JPanel(); 
+      panel4.setBackground(Color.blue);
+      JPanel panel5 = new JPanel(); 
+      panel5.setBackground(Color.red);
+      JPanel panel6 = new JPanel(); 
+      panel6.setBackground(Color.green);
+      JPanel panel7 = new JPanel(); 
+      panel7.setBackground(Color.green);
+      JPanel panel8 = new JPanel(); 
+      panel8.setBackground(Color.black);
+      JPanel panel9 = new JPanel(); 
+      panel9.setBackground(Color.white);
+      Container pane = theGUI.getContentPane();
+      pane.setLayout(new GridLayout(3, 3));
+      pane.add(panel1);
+      pane.add(panel2);
+      pane.add(panel3);
+      pane.add(panel4);
+      pane.add(panel5);
+      pane.add(panel6);
+      pane.add(panel7);
+      pane.add(panel8);
+      pane.add(panel9);
+      
+      theGUI.setVisible(true);
+   }
+}
