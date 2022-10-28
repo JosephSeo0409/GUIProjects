e47fc7dc7b38c164627a6b9d26f8204926a429e4
josseo23 <116588158+JosephSeo0409@users.noreply.github.com>
2022-10-28 18:12:11 +0000

    Added Clicker 1

--- DrawRubicks/Main.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.border.Border;
-
-public class Main {
-  public static void main(String[] args) {
-    GUIRubicks.main(args);
-  }
-}
---
+++ Main.java
@@ -0,0 +1,7 @@
+import GAME.Clicker1;
+
+class Main {
+  public static void main(String[] args) {
+    new Clicker1();
+  }
+}
